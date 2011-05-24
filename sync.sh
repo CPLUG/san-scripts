9c135d5eff0d64201b7c0eb83b686be5a9dd71b7
Corey Farwell <coreyf@rwell.org>
2011-05-23 19:16:11 -0700

    fixed some bugs

--- sync.sh
+++ sync.sh
@@ -1,26 +1,27 @@
-#!/bin/env bash
+#!/bin/bash
 
+OPTIONS="-rtlHqv --delete-after --delay-updates --safe-links"
+REPO="rsync://mirrors.kernel.org/$1"
+DEST="/media/mirror/$1"
+LCK_FLE="/var/lock/mirror/$1.lck"
+LOG_FLE="/var/log/mirror/$1.log"
+
+# Check for arguments
 if [ -z $1 ]; then
    echo "Usage: sync.sh [DISTRO]"
    exit 1
 fi
 
-OPTIONS="-rtlHqv --delete-after --delay-updates --safe-links"
-REPO="rsync://mirrors.kernel.org/$1"
-DEST="/media/mirror/$1"
-LCK_FILE="/tmp/rsync-mirror-$1.lck"
-LOG_FILE="/tmp/log-$1.log"
-
 # Make sure only 1 instance runs
-if [ -e "$LCK_FILE" ]; then
-   OTHER_PID=`cat $LCK_FILE`
-   echo "Another instance already running: $OTHER_PID"
+if [ -e "$LCK_FLE" ]; then
+   OTHER_PID=`cat $LCK_FLE`
+   echo "Another instance already running: $OTHER_PID" > $LOG_FLE 
    exit 1
 fi
-echo $$ > "$LCK_FILE"
+echo $$ > "$LCK_FLE"
 
 # Sync
-rsync $OPTIONS $REPO $DEST &> $LOG_FILE
+rsync $OPTIONS $REPO $DEST &> $LOG_FLE
 
 # Cleanup
 rm -f "$LCK_FLE"
