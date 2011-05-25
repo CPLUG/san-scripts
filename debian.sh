8133a4879e01a62106a382038011fbe403bedf54
Corey Farwell <coreyf@rwell.org>
2011-05-24 17:05:39 -0700

    initial

---
+++ debian.sh
@@ -0,0 +1,1 @@
+rsync -rtlHq --delete-after --delay-updates --safe-links rsync://mirrors.kernel.org/debian /media/mirror/debian/
---
+++ ubuntu.sh
@@ -0,0 +1,2 @@
+rsync -rtlHq --delete-after --delay-updates --safe-links rsync://mirrors.kernel.org/ubuntu /media/mirror/ubuntu/
+rsync -rtlHq --delete-after --delay-updates --safe-links rsync://mirrors.kernel.org/ubuntu-releases /media/mirror/ubuntu-releases/
