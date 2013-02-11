cc150bf367160fe26c51ce752460ac519a1c0a03
Robin Choudhury <xrobinc@gmail.com>
2013-02-10 17:40:29 -0800

    deleted:    cast.sh

--- cast.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-INRES="1920x1200"
-OUTRES="1920x1200"
-FPS="30"
-
-#ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -ac 2 -i pulse -vcodec libx264 -crf 25 -maxrate 1000k -bufsize 500k -s "$OUTRES" -acodec libmp3lame -ab 128k -ar 44100 -threads 0 -f flv file.avi
-ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -ac 2 -i pulse -vcodec libx264 -maxrate 1000k -s "$OUTRES" -acodec libmp3lame -ab 128k -ar 44100 -threads 0 -f flv file.avi
-
-
